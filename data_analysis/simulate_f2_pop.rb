99bd76b305e85613dbfd87288bca6ca5dff7d7fc
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-07-22 11:53:24 +0100

    progeny to sequence generation loop is refactored and uses either bulk_num or mutant_num based on what was provided

--- data_analysis/simulate_f2_pop.rb
+++ data_analysis/simulate_f2_pop.rb
@@ -27,9 +27,11 @@ unless pars.key?('in_vcf') && pars.key?('xovers') &&
 end
 
 # number of individuals to generate or bulk
+bulk_num = nil
 if pars.key?('progeny') || pars.key?('bulk_num')
   if pars.key?('bulk_num')
-    progeny_num = 10 * pars['bulk_num']
+    bulk_num = pars['bulk_num']
+    progeny_num = 10 * bulk_num
   else
     progeny_num = pars['progeny']
   end
@@ -138,48 +140,42 @@ def get_recomb_progeny(chrs, gametes, mutation, progeny_num)
   pval = myr.pull('chisq.test(c(mt,wt), p = c(0.25,0.75))$p.value')
   warn "mutants\t#{mutant_num}\twildtype\t#{progeny_num - mutant_num}"
   warn "Chi-squared test p value for recessive trait probability\t#{pval}"
-  progeny
+  [progeny, mutant_num]
 end
 
 gametes = get_recomb_gametes(chrs, xovers, markers)
-progeny = get_recomb_progeny(chrs, gametes, mutation, progeny_num)
+progeny, mutant_num = get_recomb_progeny(chrs, gametes, mutation, progeny_num)
 File.open("selected_progeny.yml", 'w') do |file|
   file.write progeny.to_yaml
 end
 
+# set mutant number as individuals to pool if bulk_num is nil
+bulk_num = mutant_num if bulk_num == nil
 if generate_seqs
-  # a hash for bulks
-  bulks = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
-  mut_chr = mutation.keys[0]
-  mut_pos = mutation[mut_chr]
-  mut_dir = indir + "/mutant"
-  wt_dir = indir + "/wildtype"
-  FileUtils.mkdir_p mut_dir
-  FileUtils.mkdir_p wt_dir
-  for number in 0..(progeny_num-1)
-    if progeny[number][:male][mut_chr].key?(mut_pos) and
-      progeny[number][:female][mut_chr].key?(mut_pos)
-      bulks[:mutant][number] = 1
-      sample = "mutant_progeny_" + number.to_s
-      FileUtils.chdir(mut_dir)
-    else
-      bulks[:wildtype][number] = 1
-      sample = "wildtype_progeny_" + number.to_s
-      FileUtils.chdir(wt_dir)
-    end
-    [:male, :female].each do | type |
-      out_fasta = File.open(sample + type.to_s + '.fas', 'w')
-      Bio::FastaFormat.open(in_fasta).each do |fas|
-        fas.definition += ' ' + sample
-        if progeny[number][type][fas.entry_id] == {}
-          # no change in sequence
-          out_fasta.puts fas.seq.to_fasta(fas.definition, 80)
-        else
-          fas = update_variant_to_chr(progeny[number][type], fas)
-          out_fasta.puts fas.seq.to_fasta(fas.definition, 80)
+  [:wt, :mut].each do | group |
+    dir = indir + "/pool_" + group.to_s
+    FileUtils.mkdir_p dir
+    FileUtils.chdir(dir)
+    # counter for number pooled
+    i = 0
+    progeny[group].each_key do | number |
+      sample = group.to_s + "_progeny_" + number.to_s
+      [:male, :female].each do | type |
+        out_fasta = File.open(sample + type.to_s + '.fas', 'w')
+        Bio::FastaFormat.open(in_fasta).each do |fas|
+          fas.definition += ' ' + sample
+          if progeny[group][number][type][fas.entry_id] == {}
+            # no change in sequence
+            out_fasta.puts fas.seq.to_fasta(fas.definition, 80)
+          else
+            fas = update_variant_to_chr(progeny[group][number][type], fas)
+            out_fasta.puts fas.seq.to_fasta(fas.definition, 80)
+          end
         end
+        out_fasta.close
       end
-      out_fasta.close
+      i += 1
+      break if i >= bulk_num
     end
   end
 end
